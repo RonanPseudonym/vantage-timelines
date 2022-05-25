7b6b3d2076a1c1297f7553e92d49cf383a5cd095
RonanPseudonym <ronan.underwood@icloud.com>
2022-05-25 12:27:27 -0700

    Progress on bowyer-watson

--- src/gfx/bowyer_watson3.c
+++ src/gfx/bowyer_watson3.c
@@ -1,11 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "../utils/vector.h"
 #include "../shp/types.h"
+#include "triangle.h"
 
 #include "bowyer_watson3.h"
 
+#define VAL_TO_POINTER(val) memcpy(malloc(sizeof(val)), &val, sizeof(val))
+
+/* typedef struct {
+    Point a,
+    Point b
+} Edge; */
+
 Vector *bw3_triangulate(Vector *vertices) {
-    Vector *_vertices;
-    Vector *_triangles;
+    Vector *_vertices  = VECTOR_NEW();
+    Vector *_triangles = VECTOR_NEW();
 
     for (int i = 0; i < vertices->size; i ++) vector_push(_vertices, vector_index(vertices, i));
 
@@ -29,9 +40,29 @@ Vector *bw3_triangulate(Vector *vertices) {
     const double midx      = (min_x + max_x) / 2;
     const double midy      = (min_y + max_y) / 2;
 
-    const Point v1 = (Point){midx - 20 * delta_max, midy - delta_max};
-    const Point v2 = (Point){midx, midy + 20 * delta_max};
-    const Point v3 = (Point){midx + 20 * delta_max, midy - delta_max};
+    const Point p1 = (Point){midx - 20 * delta_max, midy - delta_max};
+    const Point p2 = (Point){midx, midy + 20 * delta_max};
+    const Point p3 = (Point){midx + 20 * delta_max, midy - delta_max};
 
-    vector_push(
+    Triangle _t = (Triangle){p1, p2, p3};
+    vector_push(_triangles, &_t);
+
+    for (int i = 0; i < vertices->size; i ++) {
+        Point  *p = vector_index(vertices, i);
+
+        Vector *polygon = VECTOR_NEW();
+        Vector *bads    = VECTOR_NEW();
+
+        for (int j = 0; j < _triangles->size; j ++) {
+            Triangle *t = vector_index(_triangles, j);
+
+            if (circumcircle_contains(t, p)) {
+                vector_push(bads, t);
+
+                vector_push(polygon, VAL_TO_POINTER(((Edge){t->a, t->b})));
+                vector_push(polygon, VAL_TO_POINTER(((Edge){t->b, t->c})));
+                vector_push(polygon, VAL_TO_POINTER(((Edge){t->c, t->a})));
+            }
+        }
+    }
 }
